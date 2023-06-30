7af9b826aa4e1559720d7715cd185fdb39e97fb0
Soul-8789 <102282026+Soul-8789@users.noreply.github.com>
2023-07-01 03:38:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter></Counter> */}
+<Home></Home>
+    </div>
+  );
+}
+
+export default App;
